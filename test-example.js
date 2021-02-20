e3f7dfcacbfd8b66297bb9e6e5b99381e15db841
John Beech <johnbeech.github@mkv25.net>
2021-02-20 23:25:39 +0000

    Lint test-example.js

--- test-example.js
+++ test-example.js
@@ -1,6 +1,6 @@
 const readCredentials = require('./tests/helpers/readCredentials')
 
-async function testExample() {
+async function testExample () {
   const credentials = await readCredentials()
   process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL = credentials.client_email
   process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY = credentials.private_key
