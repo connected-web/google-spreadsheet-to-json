af7dc1264081cc7ec416ad30645e3f86629015b7
John Beech <johnbeech.github@mkv25.net>
2021-02-20 17:11:18 +0000

    Add stub harness for testing end-to-end

--- tests/integration/films.test.js
+++ tests/integration/films.test.js
@@ -1,5 +1,16 @@
+const { expect } = require('chai')
+const gsjson = require('../../')
+
 const spreadsheetId = '1G2_YLuQeKXCtpOWshqIBazzUeefuOMDZ5q10F2u9MHw'
 
 describe('Film Spreadsheet to JSON using Private Auth Credentials', () => {
-  console.log('Pending:', spreadsheetId)
+  it('should request data from a live spreadsheet, and convert to JSON', async () => {
+    console.log('Work in progress:', spreadsheetId)
+    const json = await gsjson({
+      spreadsheetId
+    })
+    expect(json).to.deep.equal({
+      something: 'something'
+    })
+  })
 })
